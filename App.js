481721c23ee4ea39588fa7e98c5ecb5ab93498bc
Stanley <48493907+sefarax@users.noreply.github.com>
2022-11-13 17:11:30 +0100

    init env variables

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LOREM } from "@env";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{ LOREM }</Text>
       <StatusBar style="auto" />
     </View>
   );
